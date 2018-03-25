b7c4d9be3bcb8336313816512632ffb3d10def95
blackjackpsp <blackjackpsp3@gmail.com>
2018-03-25 17:09:09 +0000

    Second Commit

--- NuovoBotTelegram/telegrambot.py
+++ NuovoBotTelegram/telegrambot.py
@@ -21,12 +21,17 @@ GPIO.setup(11, GPIO.OUT)
 
 def handle(msg):
     chat_id = msg['chat']['id']
-    command = msg['text']
+    #command = msg['text']
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print 'Got command: %s' % command
+    #print 'Got command: %s' % command
     if content_type=='text':
       print "Messaggio ricevuto"
       bot.sendMessage(chat_id, "Ciao ciccio come stai?")
+    if content_type == 'photo':
+        bot.download_file(msg['photo'][-1]['file_id'], './file.png')
+    if content_type == 'document':
+        bot.download_file(msg['document']['file_id'], msg['document']['file_name'])
+        #bot.download_file(msg['file_id'])
     #if command == 'on':
     #   bot.sendMessage(chat_id, on(11))
     #elif command =='off':
